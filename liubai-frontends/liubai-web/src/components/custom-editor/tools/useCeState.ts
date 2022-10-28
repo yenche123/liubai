be0cad336fd6baff20139b3e1b69e3d18aad6a64
yenche123 <yenche123@alumni.sjtu.edu.cn>
2022-10-28 13:56:03 +0800

    deving: global state

--- liubai-frontends/liubai-web/src/components/custom-editor/tools/useCeState.ts
+++ liubai-frontends/liubai-web/src/components/custom-editor/tools/useCeState.ts
@@ -1,25 +1,25 @@
 
 import { ref, ShallowRef } from "vue";
 import { TipTapEditor, EditorCoreContent } from "../../../types/types-editor";
-
+import { useGlobalStateStore } from "../../../hooks/stores/useGlobalStateStore";
 
 export function useCeState(editor: ShallowRef<TipTapEditor>) {
 
   const focused = ref(false)
+  const gs = useGlobalStateStore()
 
   const onEditorFocus = (data: EditorCoreContent) => {
     focused.value = true
+    gs.$patch({ mainInputing: true })
   }
 
   const onEditorBlur = (data: EditorCoreContent) => {
     focused.value = false
+    gs.$patch({ mainInputing: false })
   } 
 
-
   const onEditorFinish = (data: EditorCoreContent) => {
-    console.log("用户敲击了 ctrl + Enter")
-    console.log(data)
-    console.log(" ")
+    
   }
 
   return {
---
+++ liubai-frontends/liubai-web/src/hooks/stores/useGlobalStateStore.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const useGlobalStateStore = defineStore("globalState", () => {
+
+  // main-view 里的输入框是否正在输入
+  const mainInputing = ref(false)
+
+  // 全局控制器，是否能监听键盘敲击
+  const canListenKeyboard = computed(() => {
+    if(mainInputing.value) return false
+    return true
+  })
+
+  return { mainInputing, canListenKeyboard }
+})
