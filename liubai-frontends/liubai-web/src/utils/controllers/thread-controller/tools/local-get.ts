92d8a99460ad438bc1b1146f03deb84819d516d3
yenche123 <tsuiyenche@outlook.com>
2022-11-16 20:13:15 +0800

    fix: sort

--- liubai-frontends/liubai-web/src/utils/controllers/thread-controller/tools/local-get.ts
+++ liubai-frontends/liubai-web/src/utils/controllers/thread-controller/tools/local-get.ts
@@ -3,6 +3,7 @@ import time from "../../../basic/time";
 import { db } from "../../../db";
 import type { TcListOption } from "../type";
 import { fastForward } from "../../tools/util-db"
+import { ContentLocalTable } from "../../../../types/types-table";
 
 async function getList(
   opt?: TcListOption
@@ -15,49 +16,35 @@ async function getList(
     member
   } = opt ?? {}
 
-  const now = time.getTime()
-  const MIN = 1000 * 60
+  let list: ContentLocalTable[] = []
 
-  let w = "workspace+infoType+oState"
-  let b1: any[] = [workspace, "THREAD", oState]
-  let b2: any[] = [workspace, "THREAD", oState]
-  let includeLower = true
-  let includeUpper = true
-
-  if(member) {
-    w += "+member"
-    b1.push(member)
-    b2.push(member)
+  const filterFunc = (item: ContentLocalTable) => {
+    if(item.workspace === workspace && item.oState === oState) {
+      if(!member) return true
+      if(member === item.member) return true
+    }
+    return false
   }
 
-  if(lastCreatedStamp) {
-    w += "+createdStamp"
-    if(sort === "desc") {
-      b1.push(0)
-      b2.push(lastCreatedStamp)
-      includeLower = true
-      includeUpper = false
-    }
-    else {
-      b1.push(lastCreatedStamp)
-      b2.push(now + MIN)
-      includeLower = false
-      includeUpper = true
-    }
+  // 查询首页
+  if(!lastCreatedStamp) {
+    let tmp = db.contents.orderBy("createdStamp")
+    if(sort === "desc") tmp = tmp.reverse()
+    tmp = tmp.filter(filterFunc)
+    tmp = tmp.limit(10)
+    list = await tmp.toArray()
   }
-
-  w = `[${w}]`
-  let tmp = db.contents.where(w).between(b1, b2, includeLower, includeUpper)
-  tmp = tmp.limit(10)
-  if(sort === "desc") tmp = tmp.reverse()
-
-  console.time("【重要指标】local-get")
-  const res = await tmp.sortBy("createdStamp")
-  console.timeEnd("【重要指标】local-get")
-
-  console.log(" ")
-
-  return res
+  else {
+    // 查询首页以后
+    let w = db.contents.where("createdStamp")
+    let tmp = sort === "desc" ? w.below(lastCreatedStamp) : w.above(lastCreatedStamp)
+    if(sort === "desc") tmp = tmp.reverse()
+    tmp = tmp.filter(filterFunc)
+    tmp = tmp.limit(10)
+    list = await tmp.toArray()
+  }
+  
+  return list
 }
 
 function getData(
