d05db963f36130276476d7e9405fca9f08249fd0
yenche123 <tsuiyenche@outlook.com>
2022-11-23 23:06:32 +0800

    feat: search tag

--- liubai-frontends/liubai-web/src/components/custom-ui/hashtag-editor/tools/handle.ts
+++ liubai-frontends/liubai-web/src/components/custom-ui/hashtag-editor/tools/handle.ts
@@ -1,4 +1,5 @@
 import { useWorkspaceStore } from "../../../../hooks/stores/useWorkspaceStore"
+import type { TagItem } from "./types"
 import type { TagView } from "../../../../types/types-atom"
 
 // 转换文字成规范格式
@@ -29,7 +30,7 @@ export function formatTagText(val: string) {
   return list.join("/")
 }
 
-function _getTagList() {
+function _getTagList(): TagView[] {
   const store = useWorkspaceStore()
   const workspace = store.currentSpace
   if(!workspace) return []
@@ -41,13 +42,82 @@ function _getTagList() {
 /**
  * 从 useWorkspaceStore 里取数据，开始查找，返回 TagItem[]
  * 用户可能输入 yyy/zzz 那么 xxx/yyy/zzz 的结果必须找出来
- * @param text 
+ * @param text 必须已 format 过了
  */
 export async function search(text: string) {
-  const tagViews: TagView[] = []
+  const texts = text.split("/")
+  const tagList = _getTagList()
+  if(tagList.length < 1) return []
+  const list = _searchInList(texts, [], tagList)
+  return list
+}
+
+
+// 递归检测 tagViews，查看是否有匹配由关键词组成的 texts
+function _searchInList(texts: string[], parents: string[], tagViews: TagView[]) {
+  let list: TagItem[] = []
+
+  for(let i=0; i<tagViews.length; i++) {
+    const tagView = tagViews[i]
+    if(tagView.oState !== "OK") continue
+
+    const res1 = _searchInTagView(texts, parents, tagView)
+    if(res1) {
+      const obj: TagItem = {
+        tagId: tagView.tagId,
+        textBlank: parents.join(" / ") + " / " + tagView.text,
+        emoji: tagView.icon ? decodeURIComponent(tagView.icon) : undefined
+      }
+      list.push(obj)
+    }
+    else if(tagView.children) {
+      let newParents = [...parents, tagView.text]
+      const tmpList = _searchInList(texts, newParents, tagView.children)
+      list = list.concat(tmpList)
+    }
+
+  }
+
+  return list
+}
 
 
 
+/**
+ * 查找当前节点+其祖先节点，是否能完全匹配出关键词
+ * @param texts 当前文字，经 "/" 拆分所组成的关键词列表
+ * @param parents 当前节点的祖先节点之关键词列表
+ * @param tagView 当前节点
+ * @return 若有匹配则返回 true，否则为 false
+ */
+function _searchInTagView(texts: string[], parents: string[], tagView: TagView) {
+  if(texts.length > parents.length + 1) return false
+
+  if(texts.length === 1) {
+    const t1 = texts[0].toLowerCase()
+    const t2 = tagView.text.toLowerCase()
+    return t2.startsWith(t1)
+  }
+
+  let newTexts = [...texts]
+  newTexts.reverse()   // 反转
+  console.log("看一下 texts: ", texts)
+  console.log("看一下 newTexts: ", newTexts)
+  let newParents = [...parents]
+  newParents.reverse()
+
+  const list = [tagView.text, ...newParents]
+  for(let i=0; i<newTexts.length; i++) {
+    const v1 = newTexts[i].toLowerCase()
+    const v2 = list[0].toLowerCase()
+    if(v1 === v2) {
+      list.splice(0, 1)
+      continue
+    }
+    return false
+  }
+
+  return true
 }
 
 
--- liubai-frontends/liubai-web/src/components/custom-ui/hashtag-editor/tools/types.ts
+++ liubai-frontends/liubai-web/src/components/custom-ui/hashtag-editor/tools/types.ts
@@ -18,7 +18,7 @@ export type HteResolver = (res: HashTagEditorRes) => void
 
 export interface TagItem {
   tagId: string
-  text: string
+  textBlank: string       // 该字段的文字里， "/" 前后会有空格，变成 "xxxx / yyyy / zzzzz"
   emoji?: string          // 直接就是 emoji 字符串，无需编解码
 } 
 
