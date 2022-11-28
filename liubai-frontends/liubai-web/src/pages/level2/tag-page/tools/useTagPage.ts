994d4e56723232f06aa2a1a8d528ea86125e7d35
yenche123 <tsuiyenche@outlook.com>
2022-11-28 20:54:06 +0800

    feat: tag page navi

--- liubai-frontends/liubai-web/src/pages/level2/tag-page/tools/useTagPage.ts
+++ liubai-frontends/liubai-web/src/pages/level2/tag-page/tools/useTagPage.ts
@@ -1,18 +1,24 @@
-import { computed, onActivated, onDeactivated, Ref, ref, watch } from "vue";
+import { ref, watch } from "vue";
+import type { Ref } from "vue";
 import { useRouteAndLiuRouter } from "../../../../routes/liu-router";
 import type { RouteLocationNormalizedLoaded } from "vue-router";
-
-
-let isActivated = false
+import { tagIdsToShows } from "../../../../utils/system/workspace";
+import { useWorkspaceStore } from "../../../../hooks/stores/useWorkspaceStore";
+import { storeToRefs } from "pinia";
 
 export function useTagPage() {
 
   const tagName = ref("")
   const { route } = useRouteAndLiuRouter()
-  watch(() => route, (newV) => {
+  const wStore = useWorkspaceStore()
+  const { spaceId } = storeToRefs(wStore)
+
+  // 必须等 workspace 已初始化好，才能去加载 tag
+  // 因为 tag 依赖于工作区
+  watch([route, spaceId], (newV) => {
+    if(!route.name || !spaceId.value) return
     judgeTagName(tagName, route)
   })
-  judgeTagName(tagName, route)
   
   return { tagName }
 }
@@ -22,15 +28,16 @@ function judgeTagName(
   tagName: Ref<string>,
   route: RouteLocationNormalizedLoaded,
 ) {
+  console.log("## judgeTagName ##")
   if(!route) return
   const n = route.name
   if(!n) return
   if(n !== "tag" && n !== "collaborative-tag") return
-
-
-  
-  console.log("judgeTagName...........")
-
-  
-
+  const { tagId } = route.params
+  if(typeof tagId !== "string") return
+  const { tagShows } = tagIdsToShows([tagId])
+  if(!tagShows.length) return
+  const t = tagShows[0]
+  const str = `${t.emoji ? t.emoji + ' ' : '# '}${t.text}`
+  tagName.value = str
 }
