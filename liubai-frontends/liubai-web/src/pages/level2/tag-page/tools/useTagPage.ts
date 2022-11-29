31ff08654a0156e238ede910816eac138e9927c2
yenche123 <tsuiyenche@outlook.com>
2022-11-29 20:31:22 +0800

    fix: show tagName on navi

--- liubai-frontends/liubai-web/src/pages/level2/tag-page/tools/useTagPage.ts
+++ liubai-frontends/liubai-web/src/pages/level2/tag-page/tools/useTagPage.ts
@@ -16,9 +16,10 @@ export function useTagPage() {
   // 必须等 workspace 已初始化好，才能去加载 tag
   // 因为 tag 依赖于工作区
   watch([route, spaceId], (newV) => {
-    if(!route.name || !spaceId.value) return
-    judgeTagName(tagName, route)
+    judgeTagName(tagName, route, spaceId)
   })
+
+  judgeTagName(tagName, route, spaceId)
   
   return { tagName }
 }
@@ -27,14 +28,18 @@ export function useTagPage() {
 function judgeTagName(
   tagName: Ref<string>,
   route: RouteLocationNormalizedLoaded,
+  spaceId: Ref<string>,
 ) {
-  console.log("## judgeTagName ##")
   if(!route) return
+  if(!spaceId.value) return
   const n = route.name
   if(!n) return
   if(n !== "tag" && n !== "collaborative-tag") return
   const { tagId } = route.params
   if(typeof tagId !== "string") return
+
+  console.log("根据 tagId 查询 show...........")
+  console.log(" ")
   const { tagShows } = tagIdsToShows([tagId])
   if(!tagShows.length) return
   const t = tagShows[0]
