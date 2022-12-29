4781bf5c2f3b756b93bd3e3007815e3cde7e2e8a
yenche123 <tsuiyenche@outlook.com>
2022-12-29 22:15:33 +0800

    perf download

--- liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-attachments/tools/useTcAttachments.ts
+++ liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-attachments/tools/useTcAttachments.ts
@@ -20,22 +20,65 @@ export function useTcAttachments(props: TcaProps) {
     e.stopPropagation()
     if(!props.files?.length) return
     const v = props.files[index]
-
-    let url = ""
-    if(v.file) {
-      const tmpList = liuUtil.createObjURLs([v.file])
-      url = tmpList[0]
-    }
-    else if(v.cloud_url) {
-      url = v.cloud_url
-    }
-    
-    if(!url) return
-    window.open(url, "_blank")
+    checkHowToDownload(v)
   }
 
 
   return {
     onTapFile
   }
+}
+
+
+function checkHowToDownload(f: FileLocal) {
+  const _suffix = f.suffix.toLowerCase()
+
+  // pdf 文件，直接用浏览器打开查看
+  if(_suffix === "pdf") {
+    _downloadByWindow(f)
+  }
+  else {
+    _downloadByA(f)
+  }
+}
+
+function _downloadByA(f: FileLocal) {
+  let url = ""
+  let useCreateObj = false
+  if(f.file) {
+    [url] = liuUtil.createObjURLs([f.file])
+    useCreateObj = true
+  }
+  else if(f.cloud_url) {
+    url = f.cloud_url
+  }
+  if(!url) return
+
+  const a = document.createElement("a")
+  a.href = url
+  a.download = f.name
+  document.body.appendChild(a)
+  a.click()
+  a.remove()
+
+  if(useCreateObj) {
+    liuUtil.revokeObjURLs([url])
+  }
+}
+
+/**
+ * 开启新分页下载文件时，请不要使用 revokeObjURLs 回收文件
+ * 因为这样用户在新的分页时，会无法另存成新的文件
+ */
+function _downloadByWindow(f: FileLocal) {
+  let url = ""
+  if(f.file) {
+    [url] = liuUtil.createObjURLs([f.file])
+  }
+  else if(f.cloud_url) {
+    url = f.cloud_url
+  }
+
+  if(!url) return
+  window.open(url, f.name)
 }
