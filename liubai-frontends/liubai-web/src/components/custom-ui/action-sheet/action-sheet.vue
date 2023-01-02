836a6104720831e8992853684b2b93ff3fcde33a
yenche123 <tsuiyenche@outlook.com>
2023-01-02 19:04:54 +0800

    deving: actionsheet

---
+++ liubai-frontends/liubai-web/src/components/custom-ui/action-sheet/action-sheet.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+<template>
+
+  <div class="as-container"></div>
+
+</template>
+<style scoped>
+
+.as-container {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 5050;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+
+
+
+
+</style>
---
+++ liubai-frontends/liubai-web/src/components/custom-ui/action-sheet/index.ts
@@ -0,0 +1,86 @@
+import { reactive, ref } from "vue"
+import valTool from "../../../utils/basic/val-tool"
+
+interface ActionSheetItem {
+  text_key: string
+  color?: string
+}
+
+interface ActionSheetParam {
+  title_key?: string
+  itemList: ActionSheetItem[]
+  cancel_key?: string          // 自定义 "取消" 文案
+}
+
+interface AsSuccessRes {
+  result: "option" | "mask" | "cancel_btn"     // 点击了 "选项" / "蒙层" / "取消按钮"
+  tapIndex?: number
+}
+
+type AsResolver = (res: AsSuccessRes) => void
+
+const TRANSITION_DURATION = 120
+const enable = ref(false)
+const show = ref(false)
+const asData = reactive<Required<ActionSheetParam>>({
+  title_key: "",
+  itemList: [],
+  cancel_key: "",
+})
+let _resolve: AsResolver | undefined
+
+export function initActionSheet() {
+  return {
+    TRANSITION_DURATION,
+    enable,
+    show,
+    asData,
+    onTapMask,
+    onTapCancel,
+    onTapItem
+  }
+}
+
+export async function showActionSheet(opt: ActionSheetParam) {
+  asData.title_key = opt.title_key ?? ""
+  asData.itemList = opt.itemList
+  asData.cancel_key = opt.cancel_key ?? ""
+
+  await _open()
+
+  const _wait = (a: AsResolver): void => {
+    _resolve = a
+  }
+
+  return new Promise(_wait)
+}
+
+function onTapMask() {
+  _resolve && _resolve({ result: "mask" })
+  _close()
+}
+
+function onTapCancel() {
+  _resolve && _resolve({ result: "cancel_btn" })
+  _close()
+}
+
+function onTapItem(index: number) {
+  _resolve && _resolve({ result: "option", tapIndex: index })
+  _close()
+}
+
+
+async function _open() {
+  if(show.value) return
+  enable.value = true
+  await valTool.waitMilli(16)
+  show.value = true
+}
+
+async function _close() {
+  if(!show.value) return
+  show.value = false
+  await valTool.waitMilli(TRANSITION_DURATION)
+  enable.value = false
+}
