72aacafd42be4463b97c7fae6ec6dbedecf3cb3f
yenche123 <tsuiyenche@outlook.com>
2023-01-02 13:37:45 +0800

    deving: setWhen

--- liubai-frontends/liubai-web/src/components/level1/utils/specific-operate/when-remind.ts
+++ liubai-frontends/liubai-web/src/components/level1/utils/specific-operate/when-remind.ts
@@ -11,7 +11,28 @@ export const setWhen = async (
   userId: string,
 ) => {
   const newThread = valTool.copyObject(oldThread)
+  const { whenStamp } = newThread
+  if(!whenStamp) return
 
+  // 1. 显示 date-picker
+  const res = await cui.showDatePicker({ date: new Date(whenStamp) })
+  if(!res.confirm || !res.date) return
+
+  // 2. 将新的数据装到 newThread 里
+  newThread.whenStamp = res.date.getTime()
+  soTool.setEdit(newThread)
+
+  // 3. 通知到全局
+  const tsStore = useThreadShowStore()
+  tsStore.setUpdatedThreadShows([newThread])
+
+  // 4. 操作 db
+  const res2 = await dbOp.editWhenRemind(newThread, memberId, userId)
+
+  // 5. 展示通知 回传 promise
+  const tipPromise = cui.showSnackBar({ text_key: "tip.updated", action_key: "tip.undo" })
+
+  return { tipPromise }
 }
 
 export const setRemind = async (
@@ -20,6 +41,11 @@ export const setRemind = async (
   userId: string,
 ) => {
   const newThread = valTool.copyObject(oldThread)
+  const { remindMe, remindStamp } = newThread
+  if(!remindMe) return
+
+  // 展示 actionsheet
+  
 
 }
 
