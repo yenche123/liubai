898b3e2e8f369ecd643d5067384233c87693001a
yenche123 <tsuiyenche@outlook.com>
2023-01-18 15:02:21 +0800

    feat: state layout

--- liubai-frontends/liubai-web/src/pages/level2/state-page/state-page.vue
+++ liubai-frontends/liubai-web/src/pages/level2/state-page/state-page.vue
@@ -2,9 +2,6 @@
 import MainView from "~/views/main-view/main-view.vue";
 import ViceView from "~/views/vice-view/vice-view.vue";
 import ScrollView from "~/components/common/scroll-view/scroll-view.vue";
-import KanbanContent from "./kanban-content/kanban-content.vue";
-import NaviBar from "~/components/common/navi-bar/navi-bar.vue";
-import NaviVirtual from '~/components/common/navi-virtual/navi-virtual.vue';
 import { useMainVice } from "~/hooks/useMainVice";
 import { useI18n } from "vue-i18n";
 
@@ -17,10 +14,8 @@ const { t } = useI18n()
   <!-- 主视图 -->
   <main-view>
     <scroll-view>
-      <navi-virtual></navi-virtual>
-      <KanbanContent></KanbanContent>
+      你好
     </scroll-view>
-    <navi-bar :title="t('common.kanban')"></navi-bar>
   </main-view>
 
   <!-- 副视图 -->
