8ce9c811a2c4ef12d3815b49407d76e516acd03f
yenche123 <tsuiyenche@outlook.com>
2023-01-23 21:39:19 +0800

    feat: vibrate when collecting

--- liubai-frontends/liubai-web/src/components/level1/utils/specific-operate/collect.ts
+++ liubai-frontends/liubai-web/src/components/level1/utils/specific-operate/collect.ts
@@ -2,6 +2,7 @@ import { useThreadShowStore } from "~/hooks/stores/useThreadShowStore"
 import type { ThreadShow } from "~/types/types-content"
 import time from "~/utils/basic/time"
 import valTool from "~/utils/basic/val-tool"
+import liuApi from "~/utils/liu-api"
 import cui from "../../../custom-ui"
 import dbOp from "../db-op"
 
@@ -29,7 +30,10 @@ export const toCollect = async (
   const text_key = newFavorite ? "tip.collected" : "tip.canceled"
   const tipPromise = cui.showSnackBar({ text_key, action_key: "tip.undo" })
 
-  // 5. 【待完善】将该操作塞入远端待同步的队列
+  // 5. 震动
+  const vib = liuApi.vibrate([50])
+
+  // 6. 【待完善】将该操作塞入远端待同步的队列
 
   return { tipPromise, newFavorite }
 }
