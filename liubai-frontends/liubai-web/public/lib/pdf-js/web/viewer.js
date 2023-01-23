0a5faa74b9d78cb47506e0ab7d8077e7585af61e
yenche123 <tsuiyenche@outlook.com>
2023-01-23 15:20:12 +0800

    chore pdf-js

--- liubai-frontends/liubai-web/public/lib/pdf-js/web/viewer.js
+++ liubai-frontends/liubai-web/public/lib/pdf-js/web/viewer.js
@@ -2722,9 +2722,9 @@ exports.PDFViewerApplication = PDFViewerApplication;
         return;
       }
       const fileOrigin = new URL(file, window.location.href).origin;
-      if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
-      }
+      // if (fileOrigin !== viewerOrigin) {
+      //   throw new Error("file origin does not match viewer's");
+      // }
     } catch (ex) {
       PDFViewerApplication.l10n.get("loading_error").then(msg => {
         PDFViewerApplication._documentError(msg, {
