a9eade271e51ca03ffaa35e530cb35467e8da4dc
yenche123 <tsuiyenche@outlook.com>
2023-02-17 20:14:14 +0800

    feat: explanation for connect-page

--- liubai-frontends/liubai-web/src/pages/level2/connect-page/connect-content/connect-content.vue
+++ liubai-frontends/liubai-web/src/pages/level2/connect-page/connect-content/connect-content.vue
@@ -3,9 +3,36 @@
 </script>
 <template>
 
-  <span>这里是连接页</span>
+  <div class="cc-box">
+    <img src="/pics/face_with_peeking_eye_color.svg" class="cc-img" />
+    <div class="cc-text">
+      <span>这可能需要一点时间。外部应用众多，通信格式也各异，我们会试图寻找优雅的方式让人们可以很轻松的连接各个应用，同时免除琐碎配置的烦恼。这需要我们花大量的时间去研究交互，希望能得到你的理解。</span>
+    </div>
+  </div>
 
 </template>
 <style scoped>
+
+.cc-box {
+  width: 60%;
+  padding-inline: 20%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.cc-img {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 100px;
+}
+
+.cc-text {
+  text-align: center;
+  line-height: 1.9;
+  padding-block-end: 100px;
+}
 
 </style>
