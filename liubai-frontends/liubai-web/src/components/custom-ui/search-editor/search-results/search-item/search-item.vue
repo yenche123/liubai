35ce52c44db38440514218d567dfa44d46432245
yenche123 <tsuiyenche@outlook.com>
2023-02-19 16:28:23 +0800

    feat: search list ui

---
+++ liubai-frontends/liubai-web/src/components/custom-ui/search-editor/search-results/search-item/search-item.vue
@@ -0,0 +1,219 @@
+<script setup lang="ts">
+import { computed, PropType } from 'vue';
+import { useI18n } from 'vue-i18n';
+import type {
+  ScContentAtom,
+  ScRecentAtom,
+  ScThirdPartyAtom,
+} from "~/utils/controllers/search-controller/types"
+
+const props = defineProps({
+  siType: {
+    type: String as PropType<"suggest" | "recent" | "results" | "third_party">,
+    required: true
+  },
+  atomId: {
+    type: String,
+    required: true,
+  },
+  indicator: {
+    type: String,
+    required: true,
+  },
+  contentAtom: {
+    type: Object as PropType<ScContentAtom>,
+  },
+  recentAtom: {
+    type: Object as PropType<ScRecentAtom>,
+  },
+  thirdAtom: {
+    type: Object as PropType<ScThirdPartyAtom>,
+  }
+})
+
+const { t } = useI18n()
+
+const desc = computed(() => {
+  const { siType, contentAtom } = props
+  if(siType === 'suggest' || siType === 'results') {
+    if(contentAtom?.desc) return contentAtom.desc
+  }
+
+  return ""
+})
+
+const iconColor = "var(--main-code)"
+
+</script>
+<template>
+  <div class="si-container"
+    :class="{ 'si-container_selected': indicator && indicator === atomId }"
+  >
+
+    <!-- 图片框 -->
+    <div class="si-img-box">
+
+      <!-- 最近时，放大镜-->
+      <svg-icon v-if="siType === 'recent'"
+        class="si-search-icon"
+        name="search"
+        :color="iconColor"
+      ></svg-icon>
+
+      <!-- 第三方 -->
+      <template v-if="siType === 'third_party' && thirdAtom">
+        <svg-icon
+          :name="'logos-' + thirdAtom.atomId"
+          class="si-search-icon"
+          :class="{ 'si-search-icon_bing': atomId === 'bing' }"
+          :color="iconColor"
+        ></svg-icon>
+      
+      </template>
+
+      <!-- 如果有图片 -->
+      <liu-img v-else-if="contentAtom?.imgShow"
+        class="si-img"
+        :src="contentAtom.imgShow.src"
+        :blurhash="contentAtom.imgShow.blurhash"
+        border-radius="4px"
+      ></liu-img>
+
+      <!-- 评论 -->
+      <svg-icon v-else-if="contentAtom?.commentId"
+        class="si-search-icon"
+        name="comment_400"
+        :color="iconColor"
+      ></svg-icon>
+
+
+      <!-- 以 desc 兜底 -->
+      <svg-icon v-else
+        class="si-search-icon"
+        name="desc_400"
+        :color="iconColor"
+      ></svg-icon>
+
+    </div>
+
+    <div class="si-main">
+
+      <!-- 标题 -->
+      <div class="si-title">
+        <span v-if="contentAtom">{{ contentAtom.title }}</span>
+        <span v-else-if="recentAtom">{{ recentAtom.title }}</span>
+        <template v-else-if="thirdAtom">
+          <span v-if="atomId === 'bing'">{{ t('search_related.bing_search') }}</span>
+          <span v-else-if="atomId === 'xhs'">{{ t('search_related.xhs_search') }}</span>
+          <span v-else-if="atomId === 'github'">{{ t('search_related.github_search') }}</span>
+        </template>
+      </div>
+
+      <!--内文-->
+      <div v-if="desc" class="si-desc">
+        <span>{{ desc }}</span>
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+<style lang="scss" scoped>
+
+.si-container {
+  width: 100%;
+  border-radius: 4px;
+  margin-block-end: 4px;
+  padding-block: 5px;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-start;
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+}
+
+.si-container::before {
+  transition: .1s;
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--bg-color);
+  opacity: 0;
+}
+
+.si-container_selected {
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, .03);
+}
+
+.si-container_selected::before {
+  opacity: .6;
+}
+
+.si-img-box {
+  width: 60px;
+  height: 48px;
+  flex: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+.si-search-icon {
+  width: 32px;
+  height: 32px;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.si-search-icon_bing {
+  margin-inline-end: 4px;
+  margin-block-end: 4px;
+}
+
+.si-img {
+  width: 32px;
+  height: 32px;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.si-main {
+  flex: 1;
+  padding-inline-end: 8px;
+  min-height: 48px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+}
+
+.si-title {
+  font-size: var(--btn-font);
+  color: var(--main-normal);
+  max-width: 100%;
+  line-height: 1.2;
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-block: 4px;
+}
+
+.si-desc {
+  font-size: var(--mini-font);
+  color: var(--liu-quote);
+  max-width: 100%;
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-block-end: 4px;
+}
+
+</style>
--- liubai-frontends/liubai-web/src/components/custom-ui/search-editor/search-results/search-results.vue
+++ liubai-frontends/liubai-web/src/components/custom-ui/search-editor/search-results/search-results.vue
@@ -1,21 +1,148 @@
 <script setup lang="ts">
-import type { PropType } from 'vue';
+import { computed } from 'vue';
+import type { PropType } from "vue";
+import { useI18n } from 'vue-i18n';
 import type { SearchEditorData } from "../tools/types";
+import SearchItem from './search-item/search-item.vue';
 
-defineProps({
+const props = defineProps({
   seData: {
     type: Object as PropType<SearchEditorData>,
     required: true
   }
 })
 
+const show = computed(() => {
+  const { suggestList, recentList, trimTxt } = props.seData
+  if(suggestList.length) return true
+  if(recentList.length) return true
+  if(trimTxt) return true
+  return false
+})
+
+const { t } = useI18n()
+
 </script>
 <template>
 
+  <div class="sr-container" v-if="show">
 
+    <!-- 输入框为空时 -->
+    <div class="sr-box" v-show="!seData.trimTxt">
+
+      <!-- 建议 -->
+      <div class="sr-inner-box" v-if="seData.suggestList.length">
+        <div class="sri-title">
+          <span>{{ t('search_related.suggest') }}</span>
+        </div>
+        <template v-for="(item, index) in seData.suggestList" 
+          :key="item.atomId"
+        >
+          <SearchItem
+            si-type="suggest"
+            :atom-id="item.atomId"
+            :content-atom="item"
+            :indicator="seData.indicator"
+          ></SearchItem>
+        </template>
+      </div>
+
+      <!-- 最近 -->
+      <div class="sr-inner-box" v-if="seData.recentList.length">
+        <div class="sri-title">
+          <span>{{ t('search_related.recent') }}</span>
+        </div>
+        <template v-for="(item, index) in seData.recentList" 
+          :key="item.atomId"
+        >
+          <SearchItem
+            si-type="recent"
+            :atom-id="item.atomId"
+            :recent-atom="item"
+            :indicator="seData.indicator"
+          ></SearchItem>
+        </template>
+      </div>
+
+    </div>
+
+    <!-- 输入框有文字时 -->
+    <div class="sr-box" v-show="seData.trimTxt">
+
+      <!-- 搜索结果 -->
+      <div class="sr-inner-box" v-if="seData.innerList.length">
+        <div class="sri-title">
+          <span>{{ t('search_related.results') }}</span>
+        </div>
+        <template v-for="(item, index) in seData.innerList" 
+          :key="item.atomId"
+        >
+          <SearchItem
+            si-type="results"
+            :atom-id="item.atomId"
+            :content-atom="item"
+            :indicator="seData.indicator"
+          ></SearchItem>
+        </template>
+      </div>
+
+      <!-- 第三方 -->
+      <div class="sr-inner-box" v-if="seData.thirdList.length">
+        <div class="sri-title">
+          <span>{{ t('search_related.third_party') }}</span>
+        </div>
+        <template v-for="(item, index) in seData.thirdList" 
+          :key="item.atomId"
+        >
+          <SearchItem
+            si-type="third_party"
+            :atom-id="item.atomId"
+            :third-atom="item"
+            :indicator="seData.indicator"
+          ></SearchItem>
+        </template>
+      </div>
+    
+    </div>
+
+  </div>
 
 </template>
 <style lang="scss" scoped>
+
+.sr-container {
+  width: 100%;
+  position: relative;
+  border-top: 0.6px solid var(--main-tip);
+  overflow-y: auto;
+  max-height: max(50vh, 300px);
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--main-note);
+  }
+}
+
+.sr-box {
+  position: relative;
+  padding: 7px;
+  width: calc(100% - 14px);
+}
+
+.sr-inner-box {
+  width: 100%;
+  position: relative;
+  padding-block-start: 10px;
+}
+
+.sri-title {
+  font-weight: 700;
+  color: var(--liu-quote);
+  letter-spacing: 1px;
+  font-size: var(--mini-font);
+  margin-inline-start: 10px;
+  margin-block-end: 6px;
+}
+
 
 
 
