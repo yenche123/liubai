6c1c78e369f1a4894cd619c7551c89eb05135e3d
yenche123 <tsuiyenche@outlook.com>
2023-02-22 15:16:05 +0800

    feat: whatever

--- liubai-frontends/liubai-web/src/pages/level3/notification-page/notification-page.vue
+++ liubai-frontends/liubai-web/src/pages/level3/notification-page/notification-page.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import MainView from "~/views/main-view/main-view.vue";
+import ViceView from "~/views/vice-view/vice-view.vue";
 import ScrollView from "~/components/common/scroll-view/scroll-view.vue";
 import NotificationContent from "./notification-content/notification-content.vue";
 import NaviBar from "~/components/common/navi-bar/navi-bar.vue";
 import NaviVirtual from '~/components/common/navi-virtual/navi-virtual.vue';
+import { useMainVice } from "~/hooks/useMainVice";
 import { useI18n } from "vue-i18n";
 
+const { onVvWidthChange } = useMainVice()
 const { t } = useI18n()
 
 </script>
@@ -19,6 +22,9 @@ const { t } = useI18n()
     </scroll-view>
     <navi-bar :title="t('common.notification')"></navi-bar>
   </main-view>
+
+  <!-- 副视图 -->
+  <vice-view @widthchange="onVvWidthChange"></vice-view>
 
 </template>
 <style scoped>
