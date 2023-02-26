5ed11b8cc89ff44c70836cfe9e54bc098bea69dc
yenche123 <tsuiyenche@outlook.com>
2023-02-26 11:50:44 +0800

    feat: delete banner in trash

--- liubai-frontends/liubai-web/src/pages/level2/trash-page/trash-content/trash-content.vue
+++ liubai-frontends/liubai-web/src/pages/level2/trash-page/trash-content/trash-content.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import ThreadList from '~/components/level1/thread-list/thread-list.vue';
+
+const { t } = useI18n()
 
 </script>
 <template>
@@ -7,6 +10,11 @@ import ThreadList from '~/components/level1/thread-list/thread-list.vue';
   <div class="mc-container">
     <div class="tc-virtual"></div>
     <div class="mc-box">
+
+      <div class="trash-tip-box">
+        <span>{{ t('tip.delete_banner') }}</span>
+      </div>
+
       <thread-list
         view-type="TRASH"
       ></thread-list>
@@ -35,5 +43,32 @@ import ThreadList from '~/components/level1/thread-list/thread-list.vue';
   min-width: var(--card-min);
   position: relative;
 }
+
+.trash-tip-box {
+  width: calc(100%);
+  position: relative;
+  padding: 30px;
+  border-radius: 8px;
+  box-sizing: border-box;
+  color: var(--primary-color);
+  font-size: var(--mini-font);
+  margin-block-end: 20px;
+  overflow: hidden;
+  background-color: var(--card-bg);
+  user-select: none;
+}
+
+.trash-tip-box::before {
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--primary-color);
+  opacity: .19;
+}
+
+
 
 </style>
