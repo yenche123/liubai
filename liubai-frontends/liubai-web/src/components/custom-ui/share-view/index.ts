d4570fac83971c600b60a180ee67fec81a505d2c
yenche123 <tsuiyenche@outlook.com>
2023-03-01 10:21:43 +0800

    deving: share-view

--- liubai-frontends/liubai-web/src/components/custom-ui/share-view/index.ts
+++ liubai-frontends/liubai-web/src/components/custom-ui/share-view/index.ts
@@ -1,11 +1,14 @@
 
-import { reactive, ref } from "vue"
+import { reactive, ref, watch } from "vue"
 import type {
   ShareViewParam,
+  ShareViewRes,
   SvResolver
 } from "./tools/types"
 import { useRouteAndLiuRouter } from "~/routes/liu-router"
 import type { RouteAndLiuRouter } from "~/routes/liu-router"
+import valTool from "~/utils/basic/val-tool"
+import { openIt, closeIt, handleCuiQueryErr } from "../tools/useCuiTool"
 
 let _resolve: SvResolver | undefined
 const TRANSITION_DURATION = 150
@@ -20,15 +23,15 @@ const queryKey = "previewimage"
 let rr: RouteAndLiuRouter | undefined
 
 export function initShareView() {
+  listenRouteChange()
 
   return {
     TRANSITION_DURATION,
     enable,
     show,
     svData,
-
+    onTapMask,
   }
-
 }
 
 export function showShareView(param: ShareViewParam) {
@@ -36,9 +39,58 @@ export function showShareView(param: ShareViewParam) {
   svData.allowComment = param.allowComment ?? false
   svData.threadId = param.threadId
 
+  openIt(rr, queryKey)
+
+  const _wait = (a: SvResolver) => {
+    _resolve = a
+  }
+  return new Promise(_wait)
+}
+
+function listenRouteChange() {
+  rr = useRouteAndLiuRouter()
+  watch(rr.route, (newV) => {
+    const { query } = newV
+    if(!query) return
+
+    const { q, search } = query
+    if(search === "01" || q) {
+      if(_resolve) _toOpen()
+      else handleCuiQueryErr(rr, queryKey)
+      return
+    }
+
+    if(_resolve) {
+      toResolve(true)
+    }
+
+    _toClose()
+  })
 }
 
 
-function onTapMask() {
+function toResolve(res: ShareViewRes) {
+  if(!_resolve) return
+  _resolve(res)
+  _resolve = undefined
+}
 
+async function _toOpen() {
+  if(show.value) return
+  enable.value = true
+  await valTool.waitMilli(16)
+  show.value = true
+  await valTool.waitMilli(TRANSITION_DURATION)
+}
+
+async function _toClose() {
+  if(!enable.value) return
+  show.value = false
+  await valTool.waitMilli(TRANSITION_DURATION)
+  enable.value = false
+}
+
+function onTapMask() {
+  toResolve(true)
+  closeIt(rr, queryKey)
 }
--- liubai-frontends/liubai-web/src/components/custom-ui/share-view/share-view.vue
+++ liubai-frontends/liubai-web/src/components/custom-ui/share-view/share-view.vue
@@ -6,6 +6,7 @@ const {
   enable,
   show,
   svData,
+  onTapMask,
 } = initShareView()
 
 
