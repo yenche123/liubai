53446329a3258487be11f0381906a6c3637a364b
yenche123 <tsuiyenche@outlook.com>
2023-03-14 13:13:16 +0800

    feat: logic of two images layout

--- liubai-frontends/liubai-web/src/utils/images/tools/img-layout.ts
+++ liubai-frontends/liubai-web/src/utils/images/tools/img-layout.ts
@@ -19,7 +19,8 @@ export function getImgLayout(
     return { one }
   }
   else if(len === 2) {
-
+    let two = _handleTwoImgs(images)
+    return { two }
   }
 
 }
@@ -60,4 +61,53 @@ function _handleOneImg(
   let heightStr = `${Math.round(h2w * 100)}%`
 
   return { maxWidthPx, heightStr }
+}
+
+function _handleTwoImgs(
+  images: ImageShow[],
+): ImgTwoLayout {
+  let heightStr = ""
+  let maxWidthPx = 450
+  let isColumn = false
+
+  let horizontalNum = 0
+  let sum_h2w = 0
+  let len = images.length
+  images.forEach(v => {
+    let h2w = Number(v.h2w)
+    if(!h2w || isNaN(h2w)) h2w = 1
+    if(h2w <= 0.9) horizontalNum++
+    sum_h2w += h2w
+  })
+  if(horizontalNum === len) {
+    isColumn = true
+  }
+
+  let avg_h2w = sum_h2w / len
+  let box_h2w = 1
+  if(isColumn) {
+    // 竖直排列时
+    box_h2w = avg_h2w * 2
+  }
+  else {
+    // 水平排列时
+    box_h2w = avg_h2w / 2
+  }
+
+  if(box_h2w < 1.01) {
+    maxWidthPx = -200 * box_h2w + 600
+  }
+  else {
+    maxWidthPx = -120 * box_h2w + 470
+  }
+
+  if(maxWidthPx > 750) maxWidthPx = 750
+  if(maxWidthPx < 100) maxWidthPx = 100
+
+  if(box_h2w < 0.3) box_h2w = 0.3
+  if(box_h2w > 4) box_h2w = 4
+
+  heightStr = `${Math.round(box_h2w * 100)}%`
+
+  return { maxWidthPx, heightStr, isColumn }
 }
