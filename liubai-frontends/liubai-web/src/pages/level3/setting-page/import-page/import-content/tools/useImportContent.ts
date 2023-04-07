5c88a69346b226f7ad48e6fb5f4a2b2a859c6542
yenche123 <tsuiyenche@outlook.com>
2023-04-07 23:09:32 +0800

    deving

--- liubai-frontends/liubai-web/src/pages/level3/setting-page/import-page/import-content/tools/useImportContent.ts
+++ liubai-frontends/liubai-web/src/pages/level3/setting-page/import-page/import-content/tools/useImportContent.ts
@@ -23,11 +23,11 @@ export function useImportContent() {
   }
 }
 
-async function loadZip(file: File) {
+async function loadZip(f: File) {
 
   let results: JSZip
   try {
-    results = await JSZip.loadAsync(file)
+    results = await JSZip.loadAsync(f)
   }
   catch(err) {
     console.warn("loadAsync err: ")
@@ -38,6 +38,10 @@ async function loadZip(file: File) {
   
   console.log("结果: ")
   results.forEach((relativePath, file) => {
+    // 第二个参数 file 为 ZipObject 类型
+    // 该类型有 async 方法，用来读取文件
+    // 详情见: https://stuk.github.io/jszip/documentation/api_zipobject/async.html
+    
     console.log(relativePath)
     console.log(file)
     console.log(" ")
