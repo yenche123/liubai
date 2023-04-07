bf8616558a88f8d8f1149d75faeb5308363482b1
yenche123 <tsuiyenche@outlook.com>
2023-04-07 22:58:25 +0800

    deving: importing

--- liubai-frontends/liubai-web/src/pages/level3/setting-page/import-page/import-content/tools/useImportContent.ts
+++ liubai-frontends/liubai-web/src/pages/level3/setting-page/import-page/import-content/tools/useImportContent.ts
@@ -1,25 +1,46 @@
 import { ref } from "vue"
 import liuUtil from "~/utils/liu-util"
+import JSZip from "jszip"
+import cui from "~/components/custom-ui"
 
 export function useImportContent() {
 
   const oursFileEl = ref<HTMLInputElement | null>(null)
-
 
   const onOursFileChange = () => {
     const el = oursFileEl.value
     if(!el) return
     if(!el.files || !el.files.length) return
     const files = liuUtil.getArrayFromFileList(el.files)
-    console.log("files: ")
-    console.log(files)
-    console.log(" ")
-
+    const firstFile = files[0]
+    if(!firstFile) return
+    loadZip(firstFile)
   }
-
 
   return {
     oursFileEl,
     onOursFileChange,
   }
+}
+
+async function loadZip(file: File) {
+
+  let results: JSZip
+  try {
+    results = await JSZip.loadAsync(file)
+  }
+  catch(err) {
+    console.warn("loadAsync err: ")
+    console.log(err)
+    cui.showModal({ title_key: "tip.tip", content_key: "import.t4" })
+    return
+  }
+  
+  console.log("结果: ")
+  results.forEach((relativePath, file) => {
+    console.log(relativePath)
+    console.log(file)
+    console.log(" ")
+  })
+
 }
