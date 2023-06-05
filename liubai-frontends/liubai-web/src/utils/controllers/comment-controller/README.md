2d62dde4c012cc686333e7f5597ffb14ccd4bb5f
yenche123 <tsuiyenche@outlook.com>
2023-06-05 15:29:11 +1000

    deving: comments

--- liubai-frontends/liubai-web/src/utils/controllers/comment-controller/README.md
+++ liubai-frontends/liubai-web/src/utils/controllers/comment-controller/README.md
@@ -2,3 +2,7 @@
 
 如果已知某个目标 `targetComment` （即 `loadByComment`），还是应该把它放在 card 里，这样可以一目了然子级的所有评论。
 
+这里的 card 不一定是要白框，可以把白框去掉，表示是 comment 而非 thread。只有溯源到 thread 时，再用白框。
+
+不用白框，怎么看得出是 targetComment 呢？把内文、图片和操作栏靠左对齐到头像，视觉上放在头像之下。
+
--- liubai-frontends/liubai-web/src/utils/controllers/comment-controller/comment-controller.ts
+++ liubai-frontends/liubai-web/src/utils/controllers/comment-controller/comment-controller.ts
@@ -1,18 +1,42 @@
 // 评论加载管理器
+import { db } from "~/utils/db"
 import type { LoadByThreadOpt, LoadByCommentOpt } from "./tools/types"
-
+import type { ContentLocalTable } from "~/types/types-table"
+import { equipComments } from "../equip/comments"
 
 /**
  * 已知 threadId 加载其下的评论
+ *    这时，不要加载出已删除的评论，因为不影响阅读
+ *    加载顺序: 依时间顺序，即越早发的在越前面
  */
 export async function loadByThread(opt: LoadByThreadOpt) {
+  const { targetThread, lastItemStamp } = opt
 
-  
+  // 过滤掉 非一级的评论[和加载 lastItemStamp 以后的评论]
+  const filterFunc = (item: ContentLocalTable) => {
+    const { replyToComment, parentComment, createdStamp } = item
+    if(replyToComment || parentComment) return false
+    if(lastItemStamp) {
+      if(createdStamp < lastItemStamp) return false
+    }
+    return true
+  }
+
+  const w = {
+    parentThread: targetThread,
+    oState: "OK",
+  }
+
+  const list = await db.contents.where(w).filter(filterFunc).sortBy("createdStamp")
+  const comments = await equipComments(list)
+
+  return comments
 }
 
 
 /**
  *  已知某个目标 comment 加载它的上下文
+ *     向上加载时，必须加载出已删除的评论，这样才不会破坏 "上下文"
  */
 export async function loadByComment(opt: LoadByCommentOpt) {
   
