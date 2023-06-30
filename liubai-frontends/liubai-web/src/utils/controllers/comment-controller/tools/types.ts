372475553f3c0430f625836569cd5744b10994b7
yenche123 <tsuiyenche@outlook.com>
2023-06-30 22:07:41 +1000

    docs

--- liubai-frontends/liubai-web/src/utils/controllers/comment-controller/tools/types.ts
+++ liubai-frontends/liubai-web/src/utils/controllers/comment-controller/tools/types.ts
@@ -6,9 +6,9 @@ export interface LoadByThreadOpt {
 }
 
 export interface LoadByCommentOpt {
-  targetComment: string
+  commentId: string
   loadType: "target" | "find_children" | "find_parent"
-    // target: 仅加载 targetComment
+    // target: 仅加载 commentId 的评论
     // find_children: 加载回复 targetComment 的评论，也就是 targetComment 的一级评论
     // find_parent: 为 targetComment 溯源，直到找到 当前 thread 下的那个相符的一级、祖先评论
 
