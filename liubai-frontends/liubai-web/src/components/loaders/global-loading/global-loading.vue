f83120a52fb5a0b5c6536d2445f94e60c0516140
yenche123 <tsuiyenche@outlook.com>
2023-07-07 23:01:07 +1000

    style: adapt to Firefox

--- liubai-frontends/liubai-web/src/components/loaders/global-loading/global-loading.vue
+++ liubai-frontends/liubai-web/src/components/loaders/global-loading/global-loading.vue
@@ -23,6 +23,12 @@ const {
   height: 4px;
   z-index: 4000;
   border-radius: 0;
+  border: 0;
+  background-color: transparent;
+
+  &::-moz-progress-bar {
+    background-color: var(--primary-color);
+  }
 
   &::-webkit-progress-bar {
     background-color: transparent;
