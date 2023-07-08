3ea7b53de1e41252e750e52af1703dd30c151a96
yenche123 <tsuiyenche@outlook.com>
2023-07-08 22:36:56 +1000

    deving: comment-target ui

--- liubai-frontends/liubai-web/src/components/level2/comment-target/comment-target.vue
+++ liubai-frontends/liubai-web/src/components/level2/comment-target/comment-target.vue
@@ -5,9 +5,12 @@
 //   向下加载: find_children
 //   当向上加载到尽头时，加载 thread
 import PlaceholderView from '~/views/common/placeholder-view/placeholder-view.vue';
-import ThreadCard from "../../level1/thread-list/thread-card/thread-card.vue"
+import ThreadCard from "../../level1/thread-list/thread-card/thread-card.vue";
+import CommentCard from "../comment-card/comment-card.vue";
+import CommentEditor from "~/components/editors/comment-editor/comment-editor.vue";
+import PinwheelLoader from "../../loaders/pinwheel-loader/pinwheel-loader.vue"
 import type { PropType } from 'vue';
-import type { WhatDetail } from '~/types/other/types-custom';
+import type { WhatDetail, LocatedA } from '~/types/other/types-custom';
 import type { CommentTargetEmit } from "./tools/types";
 import { useCommentTarget } from "./tools/useCommentTarget"
 import { 
@@ -25,6 +28,9 @@ const props = defineProps({
     required: true,
   }
 })
+
+let commentEditorLocated: LocatedA = "vice-view"
+if(props.location === "detail-page") commentEditorLocated = "main-view"
 
 const emit = defineEmits<CommentTargetEmit>()
 const { ctData } = useCommentTarget(props, emit)
@@ -46,12 +52,74 @@ const { receiveOperation } = useThreadOperateInDetail()
     @newoperate="(op) => receiveOperation(op, ctData.thread)"
   ></ThreadCard>
 
-  <!-- 评论区 -->
-  <div class="ct-container">
+  <!-- 评论区: aboveList + 目标评论 + 回复框 + belowList -->
+  <div class="ct-container" v-if="ctData.state < 0 && ctData.targetComment">
+
+    <!-- 上方评论 -->
+    <template v-for="(item, index) in ctData.aboveList"
+      :key="item._id"
+    >
+      <CommentCard
+        :cs="item"
+        :location="location"
+      ></CommentCard>
+    </template>
+
+    <!-- 目标评论 -->
+    <CommentCard
+      :cs="ctData.targetComment"
+      :location="location"
+    ></CommentCard>
+
+    <!-- 回复框 -->
+    <CommentEditor
+      :located="commentEditorLocated"
+      :parent-thread="ctData.targetComment.parentThread"
+      :parent-comment="ctData.targetComment.replyToComment ?? ctData.targetComment._id"
+      :reply-to-comment="ctData.targetComment._id"
+    ></CommentEditor>
+
+    <!-- belowList -->
+    <template v-for="(item, index) in ctData.belowList"
+      :key="item._id"
+    >
+      <CommentCard
+        :cs="item"
+        :location="location"
+      ></CommentCard>
+    </template>
+
+    <!-- 底部加载 loading 和占位 -->
+    <div class="ct-bottom">
+
+      <!-- 必须有 belowList 才显示，因为 PlaceholderView 本身就有加载框了 -->
+      <PinwheelLoader 
+        v-if="ctData.belowList.length && !ctData.hasReachedBottom" 
+        color="var(--main-tip)"
+      ></PinwheelLoader>
+
+    </div>
 
 
 
   </div>
 
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+.ct-container {
+  width: 100%;
+  position: relative;
+}
+
+.ct-bottom {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+
+</style>
