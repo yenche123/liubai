75cf9a15582f84e0d7b99a7fe5590820e283bcf1
yenche123 <tsuiyenche@outlook.com>
2023-07-28 18:11:59 +1000

    deving: useCommentOperation

--- liubai-frontends/liubai-web/src/components/level2/comment-card/tools/useCommentOperation.ts
+++ liubai-frontends/liubai-web/src/components/level2/comment-card/tools/useCommentOperation.ts
@@ -1,6 +1,7 @@
 import type { CommentOperation } from "~/types/types-atom"
 import type { CommentCardProps } from "./types"
 import cui from "~/components/custom-ui"
+import { CommentShow } from "~/types/types-content"
 
 export function useCommentOperation(
   props: CommentCardProps
@@ -15,10 +16,55 @@ export function useCommentOperation(
       cui.showContentPanel({ comment: cs, onlyReaction: true })
       return
     }
+    else if(op === "comment") {
+
+    }
+    else if(op === "share") {
+
+    }
+    else if(op === "edit") {
+
+    }
+    else if(op === "delete") {
+      prepareToDelete(cs)
+    }
 
   }
   
   return {
     receiveOperation
   }
+}
+
+function handleComment(
+  cs: CommentShow,
+) {
+
+
+}
+
+function handleShare(
+  cs: CommentShow,
+) {
+
+}
+
+function handleEdit(
+  cs: CommentShow,
+) {
+
+}
+
+
+async function prepareToDelete(
+  cs: CommentShow,
+) {
+  const res = await cui.showModal({
+    title_key: "comment.delete_tip2",
+    content_key: "comment.delete_tip3",
+    confirm_key: "common.delete",
+    modalType: "warning"
+  })
+  if(!res.confirm) return
+
 }
