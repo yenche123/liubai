2fb3c84805f0f0b568c18716fdfb5a064bcc1f69
yenche123 <tsuiyenche@outlook.com>
2023-10-29 18:16:44 +1100

    feat: some types of cloud

--- liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
+++ liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
@@ -8,15 +8,48 @@ export async function main(ctx: FunctionContext) {
 }
 
 
-/*********************** 基类型 **********************/
+/*********************** 基类型、原子化类型 **********************/
 
 type BaseIsOn = "Y" | "N"
+
+type OState = "OK" | "REMOVED" | "DELETED"
 
 interface BaseTable {
   _id: string
   insertedStamp: number
   updatedStamp: number
 }
+
+/** 表示 “状态” 的原子结构 */
+interface LiuAtomState {
+  id: string
+  text?: string
+  color?: string     // 存储 # 开头的 hex，或者 --liu-state- 开头的系统颜色
+  showInIndex: boolean
+  contentIds: string[]
+  showFireworks?: boolean
+  updatedStamp: number
+  insertedStamp: number
+}
+
+/** 表示数据表里，存储 “状态” 的结构  */
+interface Cloud_StateConfig {
+  stateList?: LiuAtomState[]
+  updatedStamp: number
+}
+
+/** 表示 “标签” 的原子结构 */
+interface TagView {
+  tagId: string
+  text: string
+  icon?: string
+  oState: "OK" | "REMOVED"
+  createdStamp: number
+  updatedStamp: number
+  children?: TagView[]
+}
+
+
 
 
 /*********************** 数据表类型 **********************/
@@ -42,6 +75,15 @@ export interface Table_User extends BaseTable {
   systemTheme?: "light" | "dark"
   language: "en" | "zh-Hans" | "zh-Hant"
   systemLanguage?: string
+}
+
+/** Workspace 表 */
+export interface Table_Workspace extends BaseTable {
+  infoType: "ME" | "TEAM"
+  stateConfig?: Cloud_StateConfig
+  tagList?: TagView[]
+  oState: OState
+  owner: string
 }
 
 
