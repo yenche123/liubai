d21dcc284a95debe6c1e8d1ebbc15784ef7c0ba6
yenche123 <tsuiyenche@outlook.com>
2023-10-30 21:57:10 +1100

    feat: some types of cloud

--- liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
+++ liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
@@ -49,7 +49,50 @@ interface TagView {
   children?: TagView[]
 }
 
+/** Member表对象的配置结构 */
+export interface MemberConfig {
+  searchKeywords: string[]
+  searchTagIds?: string[]
+}
 
+/*********************** 文件图片相关 **********************/
+
+export interface Cloud_FileStore {
+  id: string
+  name: string
+  lastModified: number       // 文件最后修改的时间戳，精确到 ms
+  suffix: string             // 后缀的英文
+  size: number               // 单位为 bytes
+  mimeType: string
+  url: string
+}
+
+/** 图像的 exif 信息 */
+export interface LiuExif {
+  gps?: {
+    latitude?: string
+    longitude?: string
+    altitude?: string
+  }
+  DateTimeOriginal?: string    // 原始拍摄时间，形如 "YYYY:MM:DD hh:mm:ss"
+  HostComputer?: string     // 宿主设备，如果图片经过软件再编辑，此值可能缺省
+  Model?: string            // 拍摄时的设备，即使图片经过软件再编辑，此值仍可能存在
+}
+
+/** 图片于云端数据库内的存储结构 */
+export interface Cloud_ImageStore {
+  id: string
+  name: string
+  lastModified: number       // 文件最后修改的时间戳，精确到 ms
+  mimeType: string
+  width?: number
+  height?: number
+  h2w?: string
+  url: string
+  url_2?: string             // 低分辨率的图片地址
+  blurhash?: string
+  someExif?: LiuExif
+}
 
 
 /*********************** 数据表类型 **********************/
@@ -84,6 +127,16 @@ export interface Table_Workspace extends BaseTable {
   tagList?: TagView[]
   oState: OState
   owner: string
+}
+
+/** Member 表 */
+export interface Table_Member extends BaseTable {
+  name?: string
+  avatar?: Cloud_ImageStore
+  spaceId: string
+  user: string
+  oState: "OK" | "LEFT" | "DEACTIVATED" | "DELETED"
+  config?: MemberConfig
 }
 
 
