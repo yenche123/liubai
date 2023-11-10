8bf66e0e2f9eb5ee636f2d74847a58175bcadae4
yenche123 <tsuiyenche@outlook.com>
2023-11-11 10:30:57 +1100

    refactor: type-tool

--- liubai-frontends/liubai-web/src/utils/basic/type-tool.ts
+++ liubai-frontends/liubai-web/src/utils/basic/type-tool.ts
@@ -1,22 +1,21 @@
 /**
- * 把类型 T 中 特定的属性 K们 设置为可选的
- */
- export type PartialSth<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+* 把类型 T 中 特定的属性 K们 设置为可选的
+*/
+export type PartialSth<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
- /**
-  * 把类型 T 中 特定的属性 K们 设置为必选的
-  */
- export type RequireSth<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
+/**
+* 把类型 T 中 特定的属性 K们 设置为必选的
+*/
+export type RequireSth<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
- export type AnyFunc = (...args: any[]) => any
+export type AnyFunc = (...args: any[]) => any
 
- export type SimpleFunc = () => void
+export type SimpleFunc = () => void
 
- export type Prettify<T> = {
+export type Prettify<T> = {
   [K in keyof T]: T[K]
- } & {}
+} & {}
 
-
- export type LiuTimeout = ReturnType<typeof setTimeout> | undefined
+export type LiuTimeout = ReturnType<typeof setTimeout> | undefined
  
  
