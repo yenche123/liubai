61ffc01f43641dd7fb408329175db62536e145a0
yenche123 <tsuiyenche@outlook.com>
2023-11-23 20:01:55 +1100

    feat: getClientKeyEncrypted

---
+++ liubai-frontends/liubai-web/src/pages/level1/tools/common-utils.ts
@@ -0,0 +1,17 @@
+import liuUtil from "~/utils/liu-util";
+
+/** 拿到 RSA Public Key 之后，去生成的 AES key，并对其加密 */
+export async function getClientKeyEncrypted(
+  pem_public_key: string
+) {
+  const aesKey = await liuUtil.crypto.createKeyWithAES()
+  const client_key = `client_key_${aesKey}`
+  const pk = await liuUtil.crypto.importRsaPublicKey(pem_public_key)
+  if(!pk) {
+    console.log("创建 rsa 密钥失败")
+    
+    return ""
+  }
+  const cipherStr = await liuUtil.crypto.encryptWithRSA(pk, client_key)
+  return cipherStr
+}
