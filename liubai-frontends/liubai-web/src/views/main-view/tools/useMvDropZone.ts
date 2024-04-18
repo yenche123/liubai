b0907613aece4c3a85f40936296940b71b3b8334
yenche123 <tsuiyenche@outlook.com>
2024-04-18 14:03:39 +0800

    fix: cannot show drop zone

--- liubai-frontends/liubai-web/src/views/main-view/tools/useMvDropZone.ts
+++ liubai-frontends/liubai-web/src/views/main-view/tools/useMvDropZone.ts
@@ -8,8 +8,6 @@ import time from "~/utils/basic/time";
 export function useMvDropZone(
   props: MainViewProps
 ) {
-  const hasLeftPage = usePageLeave()
-
   const centerRef = ref<HTMLDivElement>()
   const dropFiles = ref<File[]>([])
   provide(mvFileKey, dropFiles)
@@ -26,29 +24,35 @@ export function useMvDropZone(
     if(files?.length) dropFiles.value = files 
   }
 
+  // const hasLeftPage = usePageLeave()
+  // const isCursorInWindow = ref(false)
+  // let lastCiwChangeStamp = 0
+  // watch(hasLeftPage, (newV) => {
+  //   console.log("hasLeftPage: ", newV)
+  //   lastCiwChangeStamp = time.getTime()
+  //   isCursorInWindow.value = !newV
+  // })
+
   const { isOverDropZone } = useDropZone(centerRef, onDrop)
-  
-  const isCursorInWindow = ref(false)
-  let lastCiwChangeStamp = 0
-  watch(hasLeftPage, (newV) => {
-    lastCiwChangeStamp = time.getTime()
-    isCursorInWindow.value = !newV
-  })
 
   // 不直接回传 isOverDropZone，而是套一层 _isOverDropZone
   // 为了考量 全局状态 isDragToSort (内部是否正在拖动以排序图片)
   const _isOverDropZone = ref(isOverDropZone.value)   
   watch(isOverDropZone, (newV) => {
+    // console.log("isOverDropZone: ", newV)
     if(globalState.isDragToSort) return
 
-    // 当前鼠标若在窗口内
-    if(isCursorInWindow.value) {
-      // 并且 hasLeftPage 变化的状态已经超过了 200ms
-      if(!time.isWithinMillis(lastCiwChangeStamp, 150)) {
-        return
-      }
-    }
+    // console.log("111111")
 
+    // 当前鼠标若在窗口内
+    // if(isCursorInWindow.value) {
+    //   // 并且 hasLeftPage 变化的状态已经超过了 150ms
+    //   if(!time.isWithinMillis(lastCiwChangeStamp, 150)) {
+    //     return
+    //   }
+    // }
+
+    // console.log("222222222")
     _isOverDropZone.value = newV
   })
 
