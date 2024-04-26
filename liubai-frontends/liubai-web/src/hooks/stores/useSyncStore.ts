653bb9a0bd15c99145921ab775831e2f666168b0
yenche123 <tsuiyenche@outlook.com>
2024-04-26 13:48:46 +0800

    feat: useSyncStore

--- liubai-frontends/liubai-web/src/hooks/stores/useSyncStore.ts
+++ liubai-frontends/liubai-web/src/hooks/stores/useSyncStore.ts
@@ -1,10 +1,41 @@
 // 当同步完成后
 // 触发此 store 让其通知到各个组件
 import { defineStore } from "pinia";
+import { shallowRef } from "vue";
 
-interface CsItem {
-  old_id: string
+export interface SyncStoreAtom {
+  whichType: "thread" | "comment"
+  first_id: string
   new_id: string
 }
 
-export const useGlobalStateStore = defineStore("content", () => {})
+type SyncStoreItem = Omit<SyncStoreAtom, "whichType">
+
+export const useSyncStore = defineStore("sync", () => {
+  const threads = shallowRef<SyncStoreItem[]>([])
+  const comments = shallowRef<SyncStoreItem[]>([])
+
+  const afterSync = (list: SyncStoreAtom[]) => {
+    const tmpThreads: SyncStoreItem[] = []
+    const tmpComments: SyncStoreItem[] = []
+
+    list.forEach(v => {
+      const item: SyncStoreItem = {
+        first_id: v.first_id,
+        new_id: v.new_id,
+      }
+      const wt = v.whichType
+      if(wt === "thread") tmpThreads.push(item)
+      else if(wt === "comment") tmpComments.push(item)
+    })
+    
+    threads.value = tmpThreads
+    comments.value = tmpComments
+  }
+
+  return {
+    threads,
+    comments,
+    afterSync,
+  }
+})
