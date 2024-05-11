ae3b106ad21a10479a5b3b3e76ce69f2e39457a7
yenche123 <tsuiyenche@outlook.com>
2024-05-11 15:20:56 +0800

    refactor: useGlobalEvent

--- liubai-frontends/liubai-web/src/hooks/tools/useGlobalEvent.ts
+++ liubai-frontends/liubai-web/src/hooks/tools/useGlobalEvent.ts
@@ -1,14 +1,11 @@
 // 在启动 app 时，触发一次
 // 用于监听全局事件，主要是全局键盘事件处理
-// 目前只有阻止用户 Tab 的用途
-import { storeToRefs } from "pinia"
+// 目前只有阻止用户 Tab 的用途，避免页面中的链接被聚焦，出现很丑的样式
 import { useGlobalStateStore } from "../stores/useGlobalStateStore"
 import { useKeyboard } from "../useKeyboard"
 
 export function useGlobalEvent() {
-
   const gs = useGlobalStateStore()
-  const { canListenKeyboard } = storeToRefs(gs)
 
   const whenKeyDown = (e: KeyboardEvent) => {
     const key = e.key
@@ -16,7 +13,7 @@ export function useGlobalEvent() {
     // 判断是否为 Tab，若是的话再判断是否可监听键盘
     // 若皆为是的话，阻止原生事件 "点击 Tab 聚焦到按钮或链接"
     if(key === "Tab") {
-      if(canListenKeyboard.value) {
+      if(gs.canListenKeyboard) {
         e.preventDefault()
       }
     }
