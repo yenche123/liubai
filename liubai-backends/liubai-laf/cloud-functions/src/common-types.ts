9d819b7fe99690dda8347e36d1374159accdb8f9
yenche123 <tsuiyenche@outlook.com>
2024-05-14 11:58:38 +0800

    fix: cannot update workspace

--- liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
+++ liubai-backends/liubai-laf/cloud-functions/src/common-types.ts
@@ -133,18 +133,18 @@ interface LiuAtomState {
   text?: string
   color?: string     // 存储 # 开头的 hex，或者 --liu-state- 开头的系统颜色
   showInIndex: boolean
-  contentIds: string[]
+  contentIds?: string[]
   showFireworks?: boolean
   updatedStamp: number
   insertedStamp: number
 }
 
 const Sch_LiuAtomState: BaseSchema<LiuAtomState> = vbot.object({
-  id: Sch_Id,
+  id: Sch_String_WithLength,
   text: Sch_Opt_Str,
   color: Sch_Opt_Str,
   showInIndex: vbot.boolean(),
-  contentIds: vbot.array(Sch_Id),
+  contentIds: sch_opt_arr(Sch_Id),
   showFireworks: vbot.optional(vbot.boolean()),
   updatedStamp: vbot.number(),
   insertedStamp: vbot.number()
