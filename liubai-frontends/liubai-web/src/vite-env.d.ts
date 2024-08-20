8380d4e80f45175e6b78b68bec22cf6fbd37f8cd
yenche123 <tsuiyenche@outlook.com>
2024-08-21 00:09:29 +0800

    feat: vite-plugin-pwa/client

--- liubai-frontends/liubai-web/src/vite-env.d.ts
+++ liubai-frontends/liubai-web/src/vite-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-pwa/info" />
-/// <reference types="vite-plugin-pwa/vue" />
+/// <reference types="vite-plugin-pwa/client" />
 /// <reference lib="webworker" />
 
 declare module '*.vue' {
