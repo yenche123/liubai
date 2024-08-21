d334b0c7fc9a6cff571c9de1ce3b21daa0a3578c
yenche123 <tsuiyenche@outlook.com>
2024-08-21 20:33:05 +0800

    chore: wx_reminder_tmpl

--- liubai-backends/liubai-laf/cloud-functions/common-config.ts
+++ liubai-backends/liubai-laf/cloud-functions/common-config.ts
@@ -18,7 +18,8 @@ export const wechat_tag_cfg = {
 /********************* wechat template messages ****************/
 export const wx_reminder_tmpl = {
   touser: "",
-  template_id: "2PtKG06TkCunDmMkktZLeGpEzb4xtVaZmEMN3qDALy4",
+  // template_id: "2PtKG06TkCunDmMkktZLeGpEzb4xtVaZmEMN3qDALy4",
+  template_id: "7C_Wfb_WybU8x60KRYtNxcBDesmDNIjRT-qZtzknuVo",
   url: "",
   data: {
     thing18: {
