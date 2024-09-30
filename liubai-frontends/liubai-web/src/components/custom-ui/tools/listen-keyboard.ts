6214dba17979358e67a640df67ce9470d9c1b1a6
yenche123 <tsuiyenche@outlook.com>
2024-09-30 18:11:51 +0800

    fix: type in zh-Hant and tap Enter for safari

--- liubai-frontends/liubai-web/src/components/custom-ui/tools/listen-keyboard.ts
+++ liubai-frontends/liubai-web/src/components/custom-ui/tools/listen-keyboard.ts
@@ -1,7 +1,12 @@
 
+import { isProxy, watch, type WatchStopHandle } from "vue"
+import cfg from "~/config"
+import time from "~/utils/basic/time"
 import type { KeyboardOpt } from "~/types/other/types-keyboard"
 
 let _opt: KeyboardOpt | undefined
+let _watchStop: WatchStopHandle | undefined
+let lastInputTxtChangeStamp = 0
 
 let oldInputTxt = ""
 const _handleKeyDown = (e: KeyboardEvent) => {
@@ -13,7 +18,14 @@ const _handleKeyDown = (e: KeyboardEvent) => {
   if(opt.whenKeyUp && d) {
     const key = e.key
     if(key === "Enter") {
-      oldInputTxt = d.inputTxt
+
+      // do not set oldInputTxt when inputTxt just changed within 1 frame
+      const now = time.getLocalTime()
+      const diff = now - lastInputTxtChangeStamp
+      // console.log("diff", diff)
+      if(diff > cfg.frame_duration_2) {
+        oldInputTxt = d.inputTxt
+      }
     }
   }
   
@@ -37,7 +49,11 @@ const _handleKeyUp = (e: KeyboardEvent) => {
   if(d) {
     if(key === "Enter") {
       const newInputTxt = d.inputTxt
+      // console.log("_handleKeyUp")
+      // console.log("newInputTxt", newInputTxt)
+      // console.log("oldInputTxt", oldInputTxt)
       if(newInputTxt !== oldInputTxt) {
+        oldInputTxt = newInputTxt
         return
       }
     }
@@ -67,13 +83,21 @@ export function toListenKeyboard(
   opt: KeyboardOpt,
 ) {
   _opt = opt
+
+  const d = opt.data
   
-  if(opt.data) {
-    oldInputTxt = opt.data.inputTxt
-    oldNativeTxt = opt.data.nativeInputTxt ?? ""
+  if(d) {
+    oldInputTxt = d.inputTxt
+    oldNativeTxt = d.nativeInputTxt ?? ""
+
+    if(isProxy(d)) {
+      _watchStop = watch(() => d.inputTxt, (newV) => {
+        lastInputTxtChangeStamp = time.getLocalTime()
+      })
+    }
   }
   
-  if(_opt.whenKeyDown || _opt.data) {
+  if(_opt.whenKeyDown || d) {
     window.addEventListener("keydown", _handleKeyDown)
   }
 
@@ -90,6 +114,10 @@ export function cancelListenKeyboard() {
 
   if(_opt?.whenKeyUp) {
     window.removeEventListener("keyup", _handleKeyUp)
+  }
+
+  if(_watchStop) {
+    _watchStop()
   }
 
   _opt = undefined
