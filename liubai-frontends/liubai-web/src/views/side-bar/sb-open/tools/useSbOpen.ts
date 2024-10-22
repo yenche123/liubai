0ece58aba849422387f9680d3e16f20c93c5f0cf
yenche123 <tsuiyenche@outlook.com>
2024-10-22 11:45:22 +0800

    fix: sb-open

--- liubai-frontends/liubai-web/src/views/side-bar/sb-open/tools/useSbOpen.ts
+++ liubai-frontends/liubai-web/src/views/side-bar/sb-open/tools/useSbOpen.ts
@@ -1,12 +1,25 @@
 import { storeToRefs } from "pinia"
 import { computed } from "vue"
+import cfg from "~/config"
+import { useWindowSize } from "~/hooks/useVueUse"
 import { useLayoutStore } from "~/views/useLayoutStore"
 
 export function useSbOpen() {
   const layout = useLayoutStore()
-  const { bottomNaviBar, sidebarStatus } = storeToRefs(layout)
+  const { 
+    routeHasBottomNaviBar, 
+    sidebarWidth,
+    bottomNaviBar, 
+    sidebarStatus,
+  } = storeToRefs(layout)
+  const { width } = useWindowSize()
+
   const show = computed(() => {
     if(sidebarStatus.value === "fullscreen") return false
+    if(!routeHasBottomNaviBar.value) {
+      if(sidebarWidth.value > 0) return false
+      return width.value > cfg.breakpoint_max_size.mobile
+    }
     return !Boolean(bottomNaviBar.value)
   })
   
