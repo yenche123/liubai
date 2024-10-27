444e2559a8753125423cf9eb5a8ccd59b44e4ab1
yenche123 <tsuiyenche@outlook.com>
2024-10-27 14:51:20 +0800

    perf: comment button

--- liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-actionbar/tc-actionbar.vue
+++ liubai-frontends/liubai-web/src/components/level1/thread-list/thread-card/tc-actionbar/tc-actionbar.vue
@@ -38,7 +38,7 @@ const props = defineProps({
   }
 })
 
-const emits = defineEmits<TcaEmit>()
+defineEmits<TcaEmit>()
 const { isPC } = liuApi.getCharacteristic()
 
 // decode emoji
@@ -48,15 +48,15 @@ const theEmoji = computed(() => {
   return tmp
 })
 
-// emoji最高 999+
+// emoji最高 99+
 const emojiShow = computed(() => {
-  if(props.emojiNum > 999) return "999+"
+  if(props.emojiNum > 99) return "99+"
   return `${props.emojiNum}`
 })
 
-// 评论最高 99+
+// 评论最高 9+
 const commentShow = computed(() => {
-  if(props.commentNum > 99) return "99+"
+  if(props.commentNum > 9) return "9+"
   return `${props.commentNum}`
 })
 
@@ -67,9 +67,7 @@ const default_color = "var(--main-code)"
   <div class="tca-container">
 
     <!-- 收藏 -->
-    <div v-if="spaceType === 'ME' && isMine" class="liu-hover tca-item"
-      @click.stop="$emit('tapcollect')"
-    >
+    <div v-if="spaceType === 'ME' && isMine" class="liu-hover tca-item" @click.stop="$emit('tapcollect')">
       <div class="liu-no-user-select tca-icon-box">
         <svg-icon v-show="!myFavorite" name="star" class="tca-icon_star" :color="default_color"></svg-icon>
         <svg-icon v-show="myFavorite" name="collected" class="tca-icon_collected"></svg-icon>
@@ -82,54 +80,54 @@ const default_color = "var(--main-code)"
         <span v-if="theEmoji">{{ theEmoji }}</span>
         <svg-icon v-else name="emoji" class="tca-icon_emoji" :color="default_color"></svg-icon>
       </div>
-      <div v-if="emojiNum > 0" 
-        class="liu-no-user-select tcb-text tcb-text_adjusted"
-      >
+      <div v-if="emojiNum > 0" class="liu-no-user-select tcb-text tcb-text_adjusted">
         <span>{{ emojiShow }}</span>
       </div>
     </div>
 
     <!-- tag -->
-    <div class="liu-hover tca-item"
-      @click.stop="$emit('newoperate', 'tag')"
-    >
+    <div class="liu-hover tca-item" @click.stop="$emit('newoperate', 'tag')">
       <div class="liu-no-user-select tca-icon-box">
         <svg-icon name="tag" class="tca-icon_tag" :color="default_color"></svg-icon>
       </div>
     </div>
 
     <!-- state -->
-    <div class="liu-hover tca-item"
-      @click.stop="$emit('newoperate', 'state')"
-    >
+    <div class="liu-hover tca-item" @click.stop="$emit('newoperate', 'state')">
       <div class="liu-no-user-select tca-icon-box">
         <svg-icon name="priority_400" class="tca-icon_state" :color="default_color"></svg-icon>
       </div>
     </div>
-    
-    <!-- more and more -->
+
+    <!-- comment if commentNum > 0 -->
+    <div v-if="commentNum > 0" class="liu-hover tca-item"
+      style="margin-inline-end: 4px;"
+      @click.stop="$emit('tapcomment')"
+    >
+      <div class="liu-no-user-select tca-icon-box">
+        <svg-icon name="comment" class="tca-icon_comment" :color="default_color"></svg-icon>
+      </div>
+      <div class="liu-no-user-select tcb-text tcb-text_adjusted">
+        <span>{{ commentShow }}</span>
+      </div>
+    </div>
+
+    <!-- and so on -->
     <div class="tca-more-container" v-if="isPC">
       <div class="tca-more-box" :class="{ 'tca-more-box_show': showMore }">
 
-        <!-- comment -->
-        <div class="liu-hover tca-item"
-          :style="{ 'margin-inline-end': commentNum > 0 ? '4px' : '8px' }"
+        <!-- comment if commentNum < 1 -->
+        <div v-if="commentNum < 1" class="liu-hover tca-item"
+          style="margin-inline-end: 8px;"
           @click.stop="$emit('tapcomment')"
         >
           <div class="liu-no-user-select tca-icon-box">
             <svg-icon name="comment" class="tca-icon_comment" :color="default_color"></svg-icon>
           </div>
-          <div v-if="commentNum > 0" 
-            class="liu-no-user-select tcb-text tcb-text_adjusted"
-          >
-            <span>{{ commentShow }}</span>
-          </div>
         </div>
 
         <!-- share -->
-        <div class="liu-hover tca-item"
-          @click.stop="$emit('tapshare')"
-        >
+        <div class="liu-hover tca-item" @click.stop="$emit('tapshare')">
           <div class="liu-no-user-select tca-icon-box">
             <svg-icon name="share" class="tca-icon" :color="default_color"></svg-icon>
           </div>
@@ -149,80 +147,71 @@ const default_color = "var(--main-code)"
   padding-block-start: 10px;
   display: flex;
   align-items: flex-start;
+}
 
-  .tca-item {
-    display: flex;
-    align-items: center;
-    margin-inline-end: 8px;
+.tca-item {
+  display: flex;
+  align-items: center;
+  margin-inline-end: 8px;
 
-    .tcb-text {
-      font-size: var(--btn-font);
-      color: var(--main-code);
-      padding-inline-start: 2px;
-      padding-inline-end: 8px;
-    }
-
-    .tcb-text_adjusted {
-      padding-top: 4px;
-    }
-
-  }
-
-  .tca-icon-box {
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  .tcb-text {
     font-size: var(--btn-font);
-
-    .tca-icon {
-      width: 26px;
-      height: 26px;
-    }
-
-    .tca-icon_star {
-      width: 26px;
-      height: 26px;
-    }
-
-    .tca-icon_collected {
-      width: 28px;
-      height: 28px;
-    }
-
-    .tca-icon_emoji {
-      margin-top: 3px;
-      width: 24px;
-      height: 24px;
-    }
-
-    .tca-icon_tag {
-      margin-top: 2px;
-      width: 26px;
-      height: 26px;
-    }
-
-    .tca-icon_state {
-      margin-top: 4px;
-      width: 26px;
-      height: 26px;
-    }
-
-
-    .tca-icon_comment {
-      margin-top: 5px;
-      width: 24px;
-      height: 24px;
-    }
+    color: var(--main-code);
+    padding-inline-start: 2px;
+    padding-inline-end: 8px;
   }
 
-  .tca-footer {
-    flex: 1;
-    display: flex;
-    justify-content: flex-end;
+  .tcb-text_adjusted {
+    padding-top: 4px;
+  }
+}
+
+.tca-icon-box {
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: var(--btn-font);
+
+  .tca-icon {
+    width: 26px;
+    height: 26px;
   }
 
+  .tca-icon_star {
+    width: 26px;
+    height: 26px;
+  }
+
+  .tca-icon_collected {
+    width: 28px;
+    height: 28px;
+  }
+
+  .tca-icon_emoji {
+    margin-top: 3px;
+    width: 24px;
+    height: 24px;
+  }
+
+  .tca-icon_tag {
+    margin-top: 2px;
+    width: 26px;
+    height: 26px;
+  }
+
+  .tca-icon_state {
+    margin-top: 4px;
+    width: 26px;
+    height: 26px;
+  }
+
+  .tca-icon_comment {
+    margin-top: 5px;
+    width: 24px;
+    height: 24px;
+  }
 }
 
 
