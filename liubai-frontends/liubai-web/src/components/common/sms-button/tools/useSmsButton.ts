72fe5490c2ee539b715b0e11d7dc89fb712686d3
yenche123 <tsuiyenche@outlook.com>
2024-12-17 11:19:49 +0800

    fix: sms button closed by its parent el

--- liubai-frontends/liubai-web/src/components/common/sms-button/tools/useSmsButton.ts
+++ liubai-frontends/liubai-web/src/components/common/sms-button/tools/useSmsButton.ts
@@ -1,15 +1,16 @@
-import { ref, watch, type Ref } from "vue";
+import { onBeforeUnmount, onMounted, ref, watch, type Ref } from "vue";
 import type { SmsStatus } from "~/types/types-view";
 import type { LiuInterval } from "~/utils/basic/type-tool";
 import type { SmsButtonEmit } from "./types";
 
+let lastSec = 60
 
 export function useSmsButton(
   status: Ref<SmsStatus>,
   emit: SmsButtonEmit,
 ) {
 
-  const sec = ref(60)
+  const sec = ref(lastSec)
   let timer: LiuInterval
 
   const _stopCounting = () => {
@@ -18,6 +19,7 @@ export function useSmsButton(
     }
     timer = undefined
     sec.value = 60
+    lastSec = 60
   }
 
   const _startToCount = () => {
@@ -32,6 +34,7 @@ export function useSmsButton(
         return
       }
       sec.value = newSec
+      lastSec = newSec
     }, 1000)
   }
 
@@ -40,6 +43,19 @@ export function useSmsButton(
       if(newV === "counting") {
         _startToCount()
       }
+    }
+  })
+
+  onMounted(() => {
+    if(status.value === "counting") {
+      _startToCount()
+    }
+  })
+
+  onBeforeUnmount(() => {
+    if(timer) {
+      clearInterval(timer)
+      timer = undefined
     }
   })
 
